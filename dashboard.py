853eea679ab88b1c4c2e939ea53004d709d7ea1f
matgom03 <matcrack80@gmail.com>
2025-11-24 00:08:02 -0500

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -1042,4 +1042,4 @@ app.layout = dbc.Container([
     tabs
 ], fluid=True)
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8050)))
+    app.run(host="0.0.0.0", port=8050)
